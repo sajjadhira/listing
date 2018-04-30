4032b642fd750b74c584efd7705b90f3f03b785c
Sajjad Hossain <sajjad.hira12@gmail.com>
2018-05-01 02:38:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 
+$(document).ready(function(e){});$(document).ready(function(e){var a=300,t=1200,l=700,s=e(".cd-top");e(window).scroll(function(){e(this).scrollTop()>a?s.addClass("cd-is-visible"):s.removeClass("cd-is-visible cd-fade-out"),e(this).scrollTop()>t&&s.addClass("cd-fade-out")}),s.on("click",function(a){a.preventDefault(),e("body,html").animate({scrollTop:0},l)})})
+$(document).on('click','.card-title',function(e){var base_url=$('#base_url').html();window.location.href=base_url})
+$(document).on('click','.card-img-top',function(e){var base_url=$('#base_url').html();window.location.href=base_url})
